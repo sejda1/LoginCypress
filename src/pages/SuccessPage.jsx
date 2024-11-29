d53deea6e084034fe6146df9010b5b460ce124e2
sejda <sejdaozgur@gmail.com>
2024-11-30 01:16:32 +0300

    Css duzenlemeleri yapildi

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import '../SuccessPage.css';
 
 export default function SuccessPage() {
     return (
-        <div>
-            <h1>Form Başarıyla Gönderildi!</h1>
-            <h3>Bilgileriniz kaydedildi. <br/> Teşekkür ederiz!</h3>
+        <div className='success-page'>
+            <Link to="/" className='link'>Anasayfaya Dön</Link>
+            <div className='explanation'>
             <hr/>
-            <Link to="/">Anasayfaya Dön</Link>
+            <h3>Form Başarıyla Gönderildi!</h3>
+            <br/>
+            <h3>Bilgileriniz kaydedildi.</h3>
+            <br/>
+            <h3>Teşekkür ederiz!</h3>
+            <br/>
+            <hr/>
+            </div>
         </div>
     );
 }
