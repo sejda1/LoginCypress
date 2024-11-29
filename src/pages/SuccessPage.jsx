05add331807a9dae9ec1762c04d53ee2bfdd4546
sejda <sejdaozgur@gmail.com>
2024-11-30 01:25:15 +0300

    Cypress testleri icin data-cy classlari eklendi.

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -3,17 +3,17 @@ import '../SuccessPage.css';
 
 export default function SuccessPage() {
     return (
-        <div className='success-page'>
-            <Link to="/" className='link'>Anasayfaya Dön</Link>
-            <div className='explanation'>
-            <hr/>
-            <h3>Form Başarıyla Gönderildi!</h3>
-            <br/>
-            <h3>Bilgileriniz kaydedildi.</h3>
-            <br/>
-            <h3>Teşekkür ederiz!</h3>
-            <br/>
-            <hr/>
+        <div className='success-page' data-cy="success-page">
+            <Link to="/" className='link' data-cy="back-to-home-link">Anasayfaya Dön</Link>
+            <div className='explanation' data-cy="explanation-section">
+                <hr />
+                <h3 data-cy="success-message">Form Başarıyla Gönderildi!</h3>
+                <br/>
+                <h3 data-cy="info-saved">Bilgileriniz kaydedildi.</h3>
+                <br/>
+                <h3 data-cy="thank-you-message">Teşekkür ederiz!</h3>
+                <br/>
+                <hr />
             </div>
         </div>
     );
